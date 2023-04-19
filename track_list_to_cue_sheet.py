7b9254767f94652244b56e9d78b462d3572e4838
Kar Epker <karepker@gmail.com>
2023-04-18 23:14:54 -0700

    Autoformat with yapf.

--- track_list_to_cue_sheet.py
+++ track_list_to_cue_sheet.py
@@ -5,7 +5,6 @@ Generates a cue file based on a track list.
 __author__ = 'Kar Epker'
 __copyright__ = '2016–2023, karepker@gmail.com (Kar Epker)'
 
-
 import argparse
 import csv
 import datetime
@@ -37,6 +36,7 @@ def parse_time_string(time_string):
 
     return datetime.timedelta(seconds=total_seconds)
 
+
 def parse_track_string(track):
     """Parses a track string and returns the name and time.
 
@@ -46,18 +46,17 @@ def parse_track_string(track):
     Returns: The name of the track, its duration as a timedelta, and the
         performer for the track.
     """
-    sanitized_performer_index = (args.performer_index if args.performer_index
-                                 else 0)
-    if len(track) < (max(args.name_index, args.time_index,
-                        args.performer_index if args.performer_index else 0) +
-                     1):
+    sanitized_performer_index = (args.performer_index
+                                 if args.performer_index else 0)
+    if len(track) < (max(args.name_index, args.time_index, args.performer_index
+                         if args.performer_index else 0) + 1):
         raise ValueError(
-                'Not enough fields for track {}, skipping.'.format(track))
+            'Not enough fields for track {}, skipping.'.format(track))
 
     name = track[args.name_index]
     time_string = track[args.time_index]
-    performer = (track[args.performer_index] if args.performer_index
-                 else args.performer)
+    performer = (track[args.performer_index]
+                 if args.performer_index else args.performer)
 
     logger = logging.getLogger(__name__)
     logger.debug('Got name %s, time %s, and performer %s.', name, time_string,
@@ -123,58 +122,88 @@ def create_cue_sheet(tracks):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Creates a cue sheet given '
                                      'a track list.')
-    parser.add_argument('track_list', nargs='?', type=argparse.FileType('r'),
-            default=sys.stdin, help='File to segment (default standard input).')
-    parser.add_argument('--name-index', dest='name_index', type=int,
+    parser.add_argument('track_list',
+                        nargs='?',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin,
+                        help='File to segment (default standard input).')
+    parser.add_argument('--name-index',
+                        dest='name_index',
+                        type=int,
                         required=True,
                         help='The index of the column in the track list '
                         'containing the track name.')
-    parser.add_argument('--time-index', dest='time_index', required=True,
+    parser.add_argument('--time-index',
+                        dest='time_index',
+                        required=True,
                         type=int,
                         help='The index of the column in the track list '
                         'containing the track\'s elapsed time.')
-    parser.add_argument('--performer-index', dest='performer_index',
+    parser.add_argument('--performer-index',
+                        dest='performer_index',
                         type=int,
                         help='The index of the column in the track list '
                         'containing the track\'s performers. If not given, '
                         'defaults to the performer ')
-    parser.add_argument('--performer', dest='performer', required=True,
+    parser.add_argument('--performer',
+                        dest='performer',
+                        required=True,
                         help='The performer to be attributed at the album '
-                             'level. Will also be used for individual tracks '
-                             'unless a performer index is given.')
-    parser.add_argument('--start-time', dest='start_time',
+                        'level. Will also be used for individual tracks '
+                        'unless a performer index is given.')
+    parser.add_argument('--start-time',
+                        dest='start_time',
                         type=parse_time_string,
                         default=datetime.timedelta(seconds=0),
                         help='Start time of the first track in seconds. '
-                             'Only used when timestamp is false.')
-    parser.add_argument('--end-time', dest='end_time',
+                        'Only used when timestamp is false.')
+    parser.add_argument('--end-time',
+                        dest='end_time',
                         type=parse_time_string,
                         default=datetime.timedelta(seconds=0),
                         help='End time of the last track in seconds. '
-                              'Only used when timestamp and dummy are true.')
+                        'Only used when timestamp and dummy are true.')
     parser.add_argument('--title', dest='title', help='Title of the disc.')
-    parser.add_argument('--rem', dest='rem', nargs='*', help='Rem attributes. '
+    parser.add_argument('--rem',
+                        dest='rem',
+                        nargs='*',
+                        help='Rem attributes. '
                         'Specify as, e.g. "GENRE Pop"')
-    parser.add_argument('--audio-file', dest='audio_file', required=True,
-                        type=argparse.FileType('r'),
-                        help='The audio file corresponding to cue sheet this '
-                        'script will generate. This file will be used to infer '
-                        'its name for the cue sheet FILE attribute.')
-    parser.add_argument('--output-file', dest='output_file', default=sys.stdout,
+    parser.add_argument(
+        '--audio-file',
+        dest='audio_file',
+        required=True,
+        type=argparse.FileType('r'),
+        help='The audio file corresponding to cue sheet this '
+        'script will generate. This file will be used to infer '
+        'its name for the cue sheet FILE attribute.')
+    parser.add_argument('--output-file',
+                        dest='output_file',
+                        default=sys.stdout,
                         type=argparse.FileType('w'),
                         help='The location to print the output cue file. '
                         'By default, stdout.')
-    parser.add_argument('--debug', dest='log_level', default=logging.WARNING,
-                        action='store_const', const=logging.DEBUG,
+    parser.add_argument('--debug',
+                        dest='log_level',
+                        default=logging.WARNING,
+                        action='store_const',
+                        const=logging.DEBUG,
                         help='Print debug log statements.')
-    parser.add_argument('--timestamp', dest='timestamp', action='store_true',
-                        default=False,
-                        help='Whether the times associated with each track are '
-                        'timestamps, i.e. they describe the time in the file '
-                        'when the track starts.')
-    parser.add_argument('--dummy', dest='dummy', action='store_true',
+    parser.add_argument(
+        '--timestamp',
+        dest='timestamp',
+        action='store_true',
+        default=False,
+        help='Whether the times associated with each track are '
+        'timestamps, i.e. they describe the time in the file '
+        'when the track starts.')
+    parser.add_argument('--dummy',
+                        dest='dummy',
+                        action='store_true',
                         help='Add dummy track at the end (true by default).')
-    parser.add_argument('--no-dummy', dest='dummy', action='store_false',
+    parser.add_argument('--no-dummy',
+                        dest='dummy',
+                        action='store_false',
                         help='Do not add dummy track at the end.')
 
     parser.set_defaults(dummy=True)
@@ -194,9 +223,10 @@ if __name__ == '__main__':
         output_file.writelines('TITLE {}\n'.format(args.title))
 
     audio_file_name = os.path.basename(args.audio_file.name)
-    audio_file_extension = os.path.splitext(args.audio_file.name)[1][1:].upper()
+    audio_file_extension = os.path.splitext(
+        args.audio_file.name)[1][1:].upper()
     output_file.writelines('FILE "{}" {}\n'.format(audio_file_name,
-                                                 audio_file_extension))
+                                                   audio_file_extension))
 
-    output_file.writelines(
-        '{}\n'.format(cue_entry) for cue_entry in create_cue_sheet(tracks))
+    output_file.writelines('{}\n'.format(cue_entry)
+                           for cue_entry in create_cue_sheet(tracks))
